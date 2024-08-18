b763084bf38af537a31783d8fb78edd191247a81
deepanshu verma <deepanshuverma186@gmail.com>
2024-08-17 18:16:43 -0700

    backend commit

---
+++ server.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import { ConnectDB } from './db/db.js'
+import userRouter from './routes/userRoutes.js'
+import classRouter from './routes/principalRoutes.js'
+import teacherRouter from './routes/teacherRoutes.js'
+import studentRouter from './routes/studentRoutes.js'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+const app = express()
+
+dotenv.config({})
+
+const PORT = process.env.PORT
+
+app.use(express.json())
+
+app.use(cors({
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+    methods:['GET','POST','PUT','DELETE']
+}))
+
+
+app.get('/',(req,res)=>{
+  res.send(`<h1> Server is running </h1>`)
+})
+
+app.use(cookieParser())
+
+app.use('/api/v1',userRouter)
+
+app.use('/api/v1',classRouter)
+
+app.use('/api/v1',teacherRouter)
+
+app.use('/api/v1',studentRouter)
+
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+    ConnectDB()
+})
+
+
+
+
+export default app
+
+
+
